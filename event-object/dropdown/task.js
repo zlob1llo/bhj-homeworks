ea63599a99c7a8edf33db6a48808a659137a388e
zlob1llo <zlob1llo90@gmail.com>
2024-10-11 10:44:56 +0300

    Доработака первого задания

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -2,31 +2,14 @@ const dropdownLists = document.querySelectorAll(".dropdown__item");
 const btnDropdown = document.getElementsByClassName("dropdown__value")[0];
 const menuDropdown = document.getElementsByClassName("dropdown__list")[0];
 
-
- 
-btnDropdown.addEventListener ("click", () => {
-    menuDropdown.classList.toggle("dropdown__list_active")
+btnDropdown.addEventListener("click", () => {
+  menuDropdown.classList.toggle("dropdown__list_active")
 })
 
-
-// let i = 0
-
-dropdownLists[0].addEventListener("click", function () {
-    btnDropdown.textContent = dropdownLists[0].textContent
+dropdownLists.forEach((item, index) => {
+  item.onclick = (event) => {
+    event.preventDefault()
+    btnDropdown.textContent = dropdownLists[index].textContent
     menuDropdown.classList.toggle("dropdown__list_active")
-  })
-  
-dropdownLists[1].addEventListener("click", function () {
-    btnDropdown.textContent = dropdownLists[1].textContent
-    menuDropdown.classList.toggle("dropdown__list_active")
-  })
-  
-dropdownLists[2].addEventListener("click", function () {
-    btnDropdown.textContent = dropdownLists[2].textContent
-    menuDropdown.classList.toggle("dropdown__list_active")
-  })
-dropdownLists[3].addEventListener("click", function () {
-    btnDropdown.textContent = dropdownLists[3].textContent
-    menuDropdown.classList.toggle("dropdown__list_active")
-  })
-  
+  }
+})
--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,30 +1,11 @@
 const tabsList = document.querySelectorAll('.tab');
 const tabsContentList = document.querySelectorAll('.tab__content');
 
-tabsList.forEach((tab) => tab.addEventListener('click', 
-    function(event) {
-    if (!event.target.classList.contains('tab_active')) {
-        event.target.parentElement.querySelector('.tab_active').classList.remove('tab_active');
-        event.target.classList.add('tab_active');
-    }
+tabsList.forEach((item, index) => {
+    item.onclick = () => {
+        
+        
+    console.log( "Клик")
+    console.log(index)
 
-    let tabIndex = 0;
-    let contentIndex = 0;
-    
-    for (var tab of tabsList) {
-        if (tab.classList.contains('tab_active')) break
-        tabIndex++
-    }
-
-    for (var content of tabsContentList) {
-        if (!content.classList.contains('tab__content_active') && tabIndex === contentIndex) {
-            content.classList.add('tab__content_active');
-        }
-
-        if (content.classList.contains('tab__content_active') && tabIndex !== contentIndex) {
-            content.classList.remove('tab__content_active');
-        }
-        contentIndex++
-    }
-}
-));
+   }})
