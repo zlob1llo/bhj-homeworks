2aabc149a55e3c46a5b187bafd6f241bf7b02c1e
zlob1llo <zlob1llo90@gmail.com>
2024-10-11 13:14:49 +0300

    Доработака третьего задания

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,11 +1,13 @@
-const tabsList = document.querySelectorAll('.tab');
+const tabsList = Array.from(document.querySelectorAll('.tab'));
 const tabsContentList = document.querySelectorAll('.tab__content');
-
+  
 tabsList.forEach((item, index) => {
     item.onclick = () => {
-        
-        
-    console.log( "Клик")
-    console.log(index)
-
-   }})
+      for (let i = 0; i < tabsList.length; i++) {
+        tabsList[i].classList.remove("tab_active")
+        tabsContentList[i].classList.remove("tab__content_active")
+      }
+      tabsList[index].classList.add("tab_active")
+      tabsContentList[index].classList.add("tab__content_active")
+    }
+})
