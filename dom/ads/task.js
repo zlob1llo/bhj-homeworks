733e8cdad09b9317fd5ed6d4cf2831ac2c394d02
zlob1llo <zlob1llo90@gmail.com>
2024-10-14 16:25:26 +0300

    Второе задание

---
+++ dom/ads/task.js
@@ -0,0 +1,14 @@
+const rotator = document.querySelectorAll('.rotator__case')
+
+setInterval(() => {
+    let i = 0
+    rotator[i].classList.remove('rotator__case_active')
+    if (i < rotator.length-1){
+        rotator[i].nextElementSibling.classList.add('rotator__case_active')
+        i++
+        }
+    else {
+            rotator[0].classList.add('rotator__case_active')
+            i = 0
+        }
+},1000)
