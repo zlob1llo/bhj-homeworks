e475afdac30046537a645a7472d5265049882b37
zlob1llo <zlob1llo90@gmail.com>
2024-10-14 13:38:55 +0300

    Первое задание

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,12 +1,12 @@
-window.addEventListener("scroll", function() {
-    const revealElemant = document.querySelectorAll(".reveal")
-    revealElemant.forEach(function (el) {
-      const { top, bottom } = el.getBoundingClientRect()
-  
-      if (bottom < 0 || top > window.innerHeight) {
-        el.classList.add("reveal_active")
-      } else {
-        el.classList.add("reveal_active")
-      }
-    })
-  })  
+const revealElemant = document.querySelectorAll(".reveal")
+window.addEventListener("scroll", function (el) {
+  revealElemant.forEach(function (el) {
+    el.classList.remove("reveal_active")
+    const { top, bottom } = el.getBoundingClientRect()
+    if (top < window.innerHeight) {
+      el.classList.add("reveal_active")
+    } else {
+      el.classList.remove("reveal_active")
+    }
+  })
+})
